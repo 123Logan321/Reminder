8305f647bfd68d7e24b86b7af7dbff6577ea788c
Logan Yu <loganyu438@gmail.com>
2021-08-18 21:12:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,28 @@ import asyncio
 import datetime
 
 bot = commands.Bot(command_prefix="`")
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
     print("Bot is ready")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return
+    raise error
+
+@bot.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(color=discord.Colour.blue())
+    embed.set_author(name = 'Help')
+    await ctx.send(author, embed=embed)
+
+@bot.command(pass_context=True)
+async def remind(ctx, message):
+    embed = discord.Embed(title="New Reminder!",description=("Hello"),color=discord.Color.dark_teal())
+    await ctx.send(embed=embed)
 
 bot.run("ODc3NzA2ODI4MzU0NTU1OTc0.YR2iLg.f73m8XO31jvxsxZFpurIsE9Y8NQ")
